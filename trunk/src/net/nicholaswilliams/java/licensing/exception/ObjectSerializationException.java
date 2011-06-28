51b32ce11898d0affaa4fcde3366caa2a9e01f01
nicholas.williams <nicholas.williams@4cd30dee-555c-11e0-b606-116da8b7f361>
2011-06-28 17:08:51 +0000

    Updating project copyright template and pushing update to all project files. No code or functional changes.

--- trunk/src/net/nicholaswilliams/java/licensing/exception/ObjectSerializationException.java
+++ trunk/src/net/nicholaswilliams/java/licensing/exception/ObjectSerializationException.java
@@ -1,18 +1,19 @@
 /*
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
- * 
- *	  http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * ObjectSerializationException.java from LicenseManager modified Tuesday, June 28, 2011 11:34:10 CDT (-0500).
+ *
+ * Copyright 2010-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package net.nicholaswilliams.java.licensing.exception;

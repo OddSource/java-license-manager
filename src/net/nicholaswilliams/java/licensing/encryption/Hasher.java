0fb061b8976078c97099aa2b35687577f5718742
Nick Williams <github@nicholaswilliams.net>
2011-06-28 17:08:51 +0000

    Updating project copyright template and pushing update to all project files. No code or functional changes.

--- src/net/nicholaswilliams/java/licensing/encryption/Hasher.java
+++ src/net/nicholaswilliams/java/licensing/encryption/Hasher.java
@@ -1,12 +1,13 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Hasher.java from LicenseManager modified Tuesday, June 28, 2011 11:34:10 CDT (-0500).
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2010-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,

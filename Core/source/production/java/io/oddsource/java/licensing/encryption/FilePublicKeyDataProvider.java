d350460fb458274b6144df9ebd3714d99a269371
Nick Williams <github@nicholaswilliams.net>
2019-01-23 01:11:35 -0600

    Update copyright headers

--- Core/source/production/java/io/oddsource/java/licensing/encryption/FilePublicKeyDataProvider.java
+++ Core/source/production/java/io/oddsource/java/licensing/encryption/FilePublicKeyDataProvider.java
@@ -1,13 +1,11 @@
 /*
- * FilePublicKeyDataProvider.java from LicenseManager modified Thursday, May 17, 2012 21:31:40 CDT (-0500).
- *
- * Copyright 2010-2013 the original author or authors.
+ * Copyright © 2010-2019 OddSource Code (license@oddsource.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.oddsource.java.licensing.encryption;
 
 import java.io.File;
